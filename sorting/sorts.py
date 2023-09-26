43a82a639d52f218bd9e9fd9f6fbda33e944a42c
vadimyt <pvcs@yandex.ru>
2023-09-26 08:23:54 +0300

    SortsComparison

--- sorting/sorts.py
+++ sorting/sorts.py
@@ -1,15 +1,14 @@
 import time
-import multiprocessing
+from multiprocessing import Process, Queue
+import random
 import matplotlib.pyplot as plt
 
 def CreateUnsorted(n=100):
-    import random
     array = [random.randint(0, 100) for i in range(n)]
     unsorted_file = open(r'sorting\UnSorted.txt', "w", encoding='utf-8')
     for i in range(len(array)):        
         unsorted_file.write(str(array[i])+'\n')
     unsorted_file.close()
-    pass
 
 def ReadFromUnsorted():
     try:
@@ -23,7 +22,7 @@ def ReadFromUnsorted():
     # array=list(map(lambda x:x.strip(),array))
     return array
 
-def BubleSort(array):    
+def BubleSort(array,q):    
     n=len(array)
     sorted=array[:]
     start_time = time.time()
@@ -35,14 +34,15 @@ def BubleSort(array):
                 sorted[j+1] = tmp
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print('Время выполнения пузырьковой сортировки: ', elapsed_time, '\n')
+    if q!=None:
+        q.put(elapsed_time)
+    #print('Время выполнения пузырьковой сортировки: ', elapsed_time, '\n')
     sorted_file = open(r'sorting\BubleSorted.txt', "w", encoding='utf-8')
     for i in range(n):        
         sorted_file.write(str(sorted[i]))
-    sorted_file.close()    
-    pass
+    sorted_file.close()
 
-def ShakerSort(array):
+def ShakerSort(array,q):
     n=len(array)
     sorted=array[:]
     start_time=time.time()
@@ -63,14 +63,15 @@ def ShakerSort(array):
         left += 1
     end_time=time.time()
     elapsed_time=end_time-start_time
-    print('Время выполнения сортировки шейкера: ', elapsed_time, '\n')
+    if q!=None:
+        q.put(elapsed_time)
+    #print('Время выполнения сортировки шейкера: ', elapsed_time, '\n')
     sorted_file = open(r'sorting\ShakerSorted.txt', "w", encoding='utf-8')
     for i in range(n):        
         sorted_file.write(str(sorted[i]))
     sorted_file.close()
-    pass
 
-def InsertionSort(array):
+def InsertionSort(array,q):
     n=len(array)
     sorted=array[:]
     start_time=time.time()
@@ -83,26 +84,28 @@ def InsertionSort(array):
         sorted[j+1]=key
     end_time=time.time()
     elapsed_time=end_time-start_time
-    print('Время выполнения сортировки вставками: ', elapsed_time, '\n')
+    if q!=None:
+        q.put(elapsed_time)
+    #print('Время выполнения сортировки вставками: ', elapsed_time, '\n')
     sorted_file = open(r'sorting\InsertionSorted.txt', "w", encoding='utf-8')
     for i in range(n):        
         sorted_file.write(str(sorted[i])+'\n')
     sorted_file.close()
-    pass
 
-def QuickSort(array):
+def QuickSort(array,q):
     n=len(array)
     sorted=array[:]
     start_time=time.time()
     recQuickSort(sorted,0,n-1)
     end_time=time.time()
     elapsed_time=end_time-start_time
-    print('Время выполнения быстрой сортировки: ', elapsed_time, '\n')
+    if q!=None:
+        q.put(elapsed_time)
+    #print('Время выполнения быстрой сортировки: ', elapsed_time, '\n')
     sorted_file = open(r'sorting\QuickSorted.txt', "w", encoding='utf-8')
     for i in range(n):        
         sorted_file.write(str(sorted[i]))
     sorted_file.close()
-    pass
 
 def recQuickSort(array,low,high):
     if low<high:
@@ -127,10 +130,10 @@ def partition(array,low,high):
 def TestSorts():
     CreateUnsorted(10000)
     array=ReadFromUnsorted()
-    p1=multiprocessing.Process(target=BubleSort,args=(array,))
-    p2=multiprocessing.Process(target=ShakerSort,args=(array,))    
-    p3=multiprocessing.Process(target=InsertionSort,args=(array,))  
-    p4=multiprocessing.Process(target=QuickSort,args=(array,))  
+    p1=Process(target=BubleSort,args=(array,None))
+    p2=Process(target=ShakerSort,args=(array,None))    
+    p3=Process(target=InsertionSort,args=(array,None))  
+    p4=Process(target=QuickSort,args=(array,None))  
     p1.start()
     p2.start()
     p3.start()
@@ -145,14 +148,66 @@ def TestGraph():
     y = [25, 32, 34, 20, 25]
     a = [10, 11, 12, 13, 14] 
     b = [10, 11, 12, 13, 14] 
-    plt.plot(x,y, color="g",label='per')
-    plt.plot(a,b, color="r",label='viy')
+    plt.plot(x,y, color="g",label='xy')
+    plt.plot(a,b, color="r",label='ab')
     plt.legend()
     plt.show()
 
+def SortsComparison():
+    n=10
+    m=15
+    ABS=[]
+    ASS=[]
+    AIS=[]
+    AQS=[]
+    Y=[]
+    QBS=Queue()
+    QSS=Queue()
+    QIS=Queue()
+    QQS=Queue()
+    for i in range(m):
+        #l=pow(n,i+1)
+        l=n*(i+1)*5
+        Y.append(l)
+        CreateUnsorted(l)
+        array=ReadFromUnsorted()
+        p1=Process(target=BubleSort,args=(array,QBS))
+        p2=Process(target=ShakerSort,args=(array,QSS))    
+        p3=Process(target=InsertionSort,args=(array,QIS))  
+        p4=Process(target=QuickSort,args=(array,QQS))  
+        p1.start()
+        p2.start()
+        p3.start()
+        p4.start()
+        p1.join()
+        p2.join()
+        p3.join()
+        p4.join()
+        ABS.append(QBS.get())
+        ASS.append(QSS.get())
+        AIS.append(QIS.get())
+        AQS.append(QQS.get())
+        print("Итерация номер:" + str(i))
+    QBS.close()
+    QBS.join_thread()
+    QSS.close()
+    QSS.join_thread()
+    QIS.close()
+    QIS.join_thread()
+    QQS.close()
+    QQS.join_thread()
+    plt.plot(ABS,Y, color="r",label='пузырьковая сортировка')
+    plt.plot(ASS,Y, color="g",label='сортировка шейкера')
+    plt.plot(AIS,Y, color="b",label='сортировка вставками')
+    plt.plot(AQS,Y, color="y",label='быстрая сортировка')
+    plt.legend()
+    plt.show()
+
+        
 def main():
-    #TestSorts()
-    TestGraph()
+    TestSorts()
+    #TestGraph()
+    #SortsComparison()
 
 if __name__ == '__main__':
     main()
