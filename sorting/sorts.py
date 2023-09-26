2eca56aa1b3f4971921908795540fe12bb1fec37
vadimyt <pvcs@yandex.ru>
2023-09-26 06:01:59 +0300

    QuickSort

--- sorting/sorts.py
+++ sorting/sorts.py
@@ -1,4 +1,3 @@
-import filecmp
 import time
 import multiprocessing
 
@@ -35,7 +34,7 @@ def BubleSort(array):
                 sorted[j+1] = tmp
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print('Время выполнения пузырьковой сортировки: ', elapsed_time)
+    print('Время выполнения пузырьковой сортировки: ', elapsed_time, '\n')
     sorted_file = open(r'sorting\BubleSorted.txt', "w", encoding='utf-8')
     for i in range(n):        
         sorted_file.write(str(sorted[i]))
@@ -63,7 +62,7 @@ def ShakerSort(array):
         left += 1
     end_time=time.time()
     elapsed_time=end_time-start_time
-    print('Время выполнения сортировки шейкера: ', elapsed_time)
+    print('Время выполнения сортировки шейкера: ', elapsed_time, '\n')
     sorted_file = open(r'sorting\ShakerSorted.txt', "w", encoding='utf-8')
     for i in range(n):        
         sorted_file.write(str(sorted[i]))
@@ -83,26 +82,62 @@ def InsertionSort(array):
         sorted[j+1]=key
     end_time=time.time()
     elapsed_time=end_time-start_time
-    print('Время выполнения сортировки вставками: ', elapsed_time)
+    print('Время выполнения сортировки вставками: ', elapsed_time, '\n')
     sorted_file = open(r'sorting\InsertionSorted.txt', "w", encoding='utf-8')
     for i in range(n):        
         sorted_file.write(str(sorted[i])+'\n')
     sorted_file.close()
     pass
 
+def QuickSort(array):
+    n=len(array)
+    sorted=array[:]
+    start_time=time.time()
+    recQuickSort(sorted,0,n-1)
+    end_time=time.time()
+    elapsed_time=end_time-start_time
+    print('Время выполнения быстрой сортировки: ', elapsed_time, '\n')
+    sorted_file = open(r'sorting\QuickSorted.txt', "w", encoding='utf-8')
+    for i in range(n):        
+        sorted_file.write(str(sorted[i]))
+    sorted_file.close()
+    pass
+
+def recQuickSort(array,low,high):
+    if low<high:
+        pivot=partition(array,low,high)
+        recQuickSort(array,low,pivot-1)
+        recQuickSort(array,pivot+1,high)
+
+def partition(array,low,high):
+    pivot=int(array[high])
+    i=low-1
+    for j in range(low,high):
+        if int(array[j]) <= pivot:
+            i=i+1
+            tmp=array[i]
+            array[i]=array[j]
+            array[j]=tmp
+    tmp=array[i+1]
+    array[i+1]=array[high]
+    array[high]=tmp
+    return i+1
+
 def main():
     CreateUnsorted(10000)
     array=ReadFromUnsorted()
     p1=multiprocessing.Process(target=BubleSort,args=(array,))
     p2=multiprocessing.Process(target=ShakerSort,args=(array,))    
-    p3=multiprocessing.Process(target=InsertionSort,args=(array,))    
+    p3=multiprocessing.Process(target=InsertionSort,args=(array,))  
+    p4=multiprocessing.Process(target=QuickSort,args=(array,))  
     p1.start()
     p2.start()
     p3.start()
+    p4.start()
     p1.join()
     p2.join()
     p3.join()
+    p4.join()
 
 if __name__ == '__main__':
-    #main()
-    print(filecmp.cmp(r'sorting\ShakerSorted.txt', r'sorting\InsertionSorted.txt'))
+    main()
