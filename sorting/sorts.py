11340edbf59531fb1d97f9d3eeed278dd8bced50
vadimyt <pvcs@yandex.ru>
2023-09-26 05:00:12 +0300

    Shaker sort + attempt at multiprocessing

--- sorting/sorts.py
+++ sorting/sorts.py
@@ -1,6 +1,9 @@
-def CreateUnsorted():
+import time
+import multiprocessing
+
+def CreateUnsorted(n=100):
     import random
-    array = [random.randint(0, 100) for i in range(100)]
+    array = [random.randint(0, 100) for i in range(n)]
     unsorted_file = open(r'sorting\UnSorted.txt', "w", encoding='utf-8')
     for i in range(len(array)):        
         unsorted_file.write(str(array[i])+'\n')
@@ -19,19 +22,64 @@ def ReadFromUnsorted():
     # array=list(map(lambda x:x.strip(),array))
     return array
 
-def BubleSort(array):
+def BubleSort(array):    
     n=len(array)
     sorted=array[:]
+    print('1')    
+    start_time = time.time()
     for i in range(n-1):
         for j in range(n-i-1):
             if int(sorted[j]) > int(sorted[j+1]):
                 tmp = sorted[j]
                 sorted[j] = sorted[j+1]
                 sorted[j+1] = tmp
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('Время выполнения пузырьковой сортировки: ', elapsed_time)
     sorted_file = open(r'sorting\BubleSorted.txt', "w", encoding='utf-8')
+    for i in range(n):        
+        sorted_file.write(str(sorted[i]))
+    sorted_file.close()    
+    pass
+
+def ShakerSort(array):
+    n=len(array)
+    sorted=array[:]
+    print('2')
+    start_time=time.time()
+    left=0
+    right=n-1
+    while left <= right:
+        for i in range(left, right, 1):
+            if int(sorted[i]) > int(sorted[i+1]):
+                tmp=sorted[i]
+                sorted[i]=sorted[i+1]
+                sorted[i+1]=tmp
+        right -= 1
+        for i in range(right, left, -1):
+            if int(sorted[i-1]) > int(sorted[i]):
+                tmp=sorted[i]
+                sorted[i]=sorted[i-1]
+                sorted[i-1]=tmp
+        left += 1
+    end_time=time.time()
+    elapsed_time=end_time-start_time
+    print('Время выполнения сортировки шейкера: ', elapsed_time)
+    sorted_file = open(r'sorting\ShakerSorted.txt', "w", encoding='utf-8')
     for i in range(n):        
         sorted_file.write(str(sorted[i]))
     sorted_file.close()
     pass
 
-BubleSort(ReadFromUnsorted())
+if __name__ == '__main__':
+    CreateUnsorted(5000)
+    array=ReadFromUnsorted()
+    p1=multiprocessing.Process(target=BubleSort,args=(array,))
+    p2=multiprocessing.Process(target=ShakerSort,args=(array,))
+    p2.start()
+    p1.start()
+    
+    p1.join()
+    p2.join()
+    BubleSort(array)
+    ShakerSort(array)
--- test.py
+++ test.py
@@ -1,2 +1,26 @@
-name = input("Введите имя: ")
-print("Привет,", name)
+import time
+import multiprocessing
+from multiprocessing import Process
+from multiprocessing import Pool
+
+def test(n):
+    time.sleep(2)
+    print("hru"+n)  
+    pass  
+
+def test2(n):
+    time.sleep(1)
+    print("qwa"+n)
+    pass
+
+if __name__ == '__main__':
+    p= multiprocessing.Process(target=test, args=("hru",))
+    p.start()
+    p2= multiprocessing.Process(target=test2, args=("hru2",))
+    p2.start()
+    p3= multiprocessing.Process(target=test, args=("hru3",))
+    p3.start()
+
+    p.join()
+    p2.join()
+    p3.join()
